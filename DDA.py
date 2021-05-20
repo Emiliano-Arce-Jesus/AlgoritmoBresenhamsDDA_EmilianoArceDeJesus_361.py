9ac2928cef4be7dbe30d52d553d2ab12a03cc006
Emiliano-Arce-Jesus <61399624+Emiliano-Arce-Jesus@users.noreply.github.com>
2021-05-20 15:29:41 -0500

    Add files via upload

---
+++ DDA.py
@@ -0,0 +1,12 @@
+from GraficaDDA import GraficarDDA
+def main():
+    x1= int(input('INGRESA VALOR PARA X1:\n'))
+    y1= int(input('INGRESA VALOR PARA X2:\n'))
+    x2= int(input('INGRESA VALOR PARA Y1:\n'))
+    y2= int(input('INGRESA VALOR PARA Y2:\n'))
+
+    GraficarDDA(x1, y1, x2, y2)
+
+if __name__== '__main__':
+    main()
+
